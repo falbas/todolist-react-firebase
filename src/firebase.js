07079326ed73d49a0f84fd632e716d12aea26139
falbas <farhanalbas30@gmail.com>
2022-02-24 18:52:22 +0700

    Add env

--- src/firebase.js
+++ src/firebase.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCIoe6ONIEmSxMLp2CNuXGddUpM0ekEy8k",
-  authDomain: "todolist-3324b.firebaseapp.com",
-  databaseURL: "https://todolist-3324b-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "todolist-3324b",
-  storageBucket: "todolist-3324b.appspot.com",
-  messagingSenderId: "693528049654",
-  appId: "1:693528049654:web:5750ef87355470d9dcb9d7",
-  measurementId: "G-CKGNGER28V"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
 };
 
 firebase.initializeApp(firebaseConfig);
